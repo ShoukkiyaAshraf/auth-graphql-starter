da60a62e4eee56442fb9e116be45e4086c95ddfb
T_H_E_E_X_P_L_O_R_E_R <shoukkiya59@gmail.com>
2023-07-27 10:21:10 +0530

    initial commit

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ const schema = require('./schema/schema');
 const app = express();
 
 // Replace with your Mongo Atlas URI
-const MONGO_URI = '';
+const MONGO_URI = 'mongodb+srv://shoukkiya59:YRQpDGX8VyTcgw6D@cluster0.9ohdwue.mongodb.net/?retryWrites=true&w=majority';
 if (!MONGO_URI) {
   throw new Error('You must provide a Mongo Atlas URI');
 }
