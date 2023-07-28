66edebcd167bcb8afa6e90707d507bf9f3b83c0d
T_H_E_E_X_P_L_O_R_E_R <shoukkiya59@gmail.com>
2023-07-28 15:40:44 +0530

    connecting mongodB

--- server/server.js
+++ server/server.js
@@ -7,14 +7,15 @@ const passport = require('passport');
 const passportConfig = require('./services/auth');
 const MongoStore = require('connect-mongo')(session);
 const schema = require('./schema/schema');
-const authorize = require('./user.json');
+const {username, password} = require('./user.json');
 
 
 // Create a new Express application
 const app = express();
 
 // Replace with your Mongo Atlas URI
-const MONGO_URI = 'mongodb+srv://'+authorize.username+':'+authorize.password+'@cluster0.9ohdwue.mongodb.net/?retryWrites=true&w=majority';
+const MONGO_URI = `mongodb+srv://${username}:${password}@cluster0.9ohdwue.mongodb.net/?retryWrites=true&w=majority`;
+
 if (!MONGO_URI) {
   throw new Error('You must provide a Mongo Atlas URI');
 }
